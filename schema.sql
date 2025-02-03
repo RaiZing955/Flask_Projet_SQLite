050366402fcb32f925ce00a3c29fda39a770bd47
RaiZing955 <d-kaya@outlook.fr>
2025-02-03 16:23:25 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,8 +1,30 @@
-DROP TABLE IF EXISTS clients;
-CREATE TABLE clients (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    nom TEXT NOT NULL,
-    prenom TEXT NOT NULL,
-    adresse TEXT NOT NULL
+-- Table des livres
+CREATE TABLE Livres (
+    ID_livre INTEGER PRIMARY KEY AUTOINCREMENT,
+    Titre TEXT NOT NULL,
+    Auteur TEXT NOT NULL,
+    Annee_publication INTEGER,
+    Quantite INTEGER NOT NULL
+);
+
+-- Table des utilisateurs
+CREATE TABLE Utilisateurs (
+    ID_utilisateur INTEGER PRIMARY KEY AUTOINCREMENT,
+    Nom TEXT NOT NULL,
+    Prenom TEXT NOT NULL,
+    Email TEXT UNIQUE NOT NULL,
+    Mot_de_passe TEXT NOT NULL,
+    Role TEXT CHECK (Role IN ('Admin', 'User')) NOT NULL DEFAULT 'User'
+);
+
+-- Table des emprunts
+CREATE TABLE Emprunts (
+    ID_emprunt INTEGER PRIMARY KEY AUTOINCREMENT,
+    ID_utilisateur INTEGER NOT NULL,
+    ID_livre INTEGER NOT NULL,
+    Date_emprunt DATE NOT NULL DEFAULT (DATE('now')),
+    Date_retour DATE,
+    Statut TEXT CHECK (Statut IN ('En cours', 'Terminé')) NOT NULL DEFAULT 'En cours',
+    FOREIGN KEY(ID_utilisateur) REFERENCES Utilisateurs(ID_utilisateur),
+    FOREIGN KEY(ID_livre) REFERENCES Livres(ID_livre)
 );
