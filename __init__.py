181cd2752cb149903d3948e2a600b1496f58be74
RaiZing955 <d-kaya@outlook.fr>
2025-02-03 16:40:46 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,7 @@
-from flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session
-from flask import render_template
-from flask import json
-from urllib.request import urlopen
-from werkzeug.utils import secure_filename
+from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
 
-app = Flask(__name__)                                                                                                                  
+app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions
 
 # Dictionnaire des utilisateurs pour l'authentification
@@ -14,9 +10,10 @@ utilisateurs = {
     "user": {"password": "12345", "role": "User"}
 }
 
-# Fonction pour créer une clé "authentifie" dans la session utilisateur
+# Fonction pour vérifier si un utilisateur est authentifié
 def est_authentifie():
-    return session.get('authentifie')
+    return session.get('authentifie', False)
+
 # Fonction pour vérifier si l'utilisateur est admin
 def est_admin():
     return session.get('role') == 'Admin'
@@ -27,72 +24,11 @@ def index():
     if not est_authentifie():  # Vérifier si l'utilisateur est authentifié
         return redirect(url_for('authentification'))
 
-# Rediriger vers la page appropriée en fonction du rôle
+    # Rediriger vers la page appropriée en fonction du rôle
     if est_admin():
         return redirect(url_for('admin_home'))
     else:
         return redirect(url_for('user_home'))
-
-@app.route('/lecture')
-def lecture():
-    if not est_authentifie():
-        # Rediriger vers la page d'authentification si l'utilisateur n'est pas authentifié
-        return redirect(url_for('authentification'))
-
-  # Si l'utilisateur est authentifié
-    return "<h2>Bravo, vous êtes authentifié</h2>"
-
-@app.route('/authentification', methods=['GET', 'POST'])
-def authentification():
-    if request.method == 'POST':
-        # Vérifier les identifiants
-        if request.form['username'] == 'admin' and request.form['password'] == 'password': # password à cacher par la suite
-            session['authentifie'] = True
-            # Rediriger vers la route lecture après une authentification réussie
-            return redirect(url_for('lecture'))
-        else:
-            # Afficher un message d'erreur si les identifiants sont incorrects
-            return render_template('formulaire_authentification.html', error=True)
-
-    return render_template('formulaire_authentification.html', error=False)
-
-@app.route('/fiche_client/<int:post_id>')
-def Readfiche(post_id):
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients WHERE id = ?', (post_id,))
-    data = cursor.fetchall()
-    conn.close()
-    # Rendre le template HTML et transmettre les données
-    return render_template('read_data.html', data=data)
-
-@app.route('/consultation/')
-def ReadBDD():
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients;')
-    data = cursor.fetchall()
-    conn.close()
-    return render_template('read_data.html', data=data)
-
-@app.route('/enregistrer_client', methods=['GET'])
-def formulaire_client():
-    return render_template('formulaire.html')  # afficher le formulaire
-
-@app.route('/enregistrer_client', methods=['POST'])
-def enregistrer_client():
-    nom = request.form['nom']
-    prenom = request.form['prenom']
-
-    # Connexion à la base de données
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-
-    # Exécution de la requête SQL pour insérer un nouveau client
-    cursor.execute('INSERT INTO clients (created, nom, prenom, adresse) VALUES (?, ?, ?, ?)', (1002938, nom, prenom, "ICI"))
-    conn.commit()
-    conn.close()
-    return redirect('/consultation/')  # Rediriger vers la page d'accueil après l'enregistrement
 
 # Route pour l'authentification
 @app.route('/authentification', methods=['GET', 'POST'])
@@ -259,6 +195,8 @@ def gerer_livres():
     conn.close()
 
     return render_template('user_home.html', livres=livres, emprunts=emprunts)
-    
+
+
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
